f312c566d90bcebcd4119f98b4f5abcb171938e9
Lex <domian844@gmail.com>
2021-02-05 16:22:53 +0700

    Pollution works now

--- src/main/java/com/detrav/net/ProspectingPacket.java
+++ src/main/java/com/detrav/net/ProspectingPacket.java
@@ -69,10 +69,10 @@ public class ProspectingPacket extends DetravPacket {
                 }
             } else if (packet.ptype == 2) {
                 // Fluid
-                rgba = fluidColors.get((int) meta);
+                rgba = fluidColors.get((int) meta); 
                 if (rgba == null) {
                     DetravScannerMod.proxy.sendPlayerExeption( "Unknown fluid ID = " + meta + " Please add to FluidColors.java!");
-                    rgba = new short[]{0,0,0,0};
+                    rgba = new short[]{125,123,118,0};
                 }
 
                 name = Objects.firstNonNull(
